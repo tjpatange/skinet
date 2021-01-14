49d57369b216717993f8193cfe9a60d91cfbb225
Tejashri <tejashripatange@gmail.com>
2021-01-14 14:26:18 +0100

    End of s ection16

---
+++ Infrastructure/Identity/Migrations/20210114122039_AddAddCountry.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Identity.Migrations
+{
+    public partial class AddAddCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Address",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Address");
+        }
+    }
+}
